c8fd4d89c83f5b93b04bcf03f608e463e1ff0d6b
koonwen <koonwen@gmail.com>
2021-05-22 22:23:16 +0800

    Add exercises 4-3 to 4-10 and reorganize folder

---
+++ Chapter4/4-10.c
@@ -0,0 +1,318 @@
+// Exercise 4-10
+/* An alternative organization uses getline to read an input line;
+   this makes getch and ungetch unnecessary. Revise the calculator to
+   use this approach
+*/
+
+#include <stdio.h>
+#include <stdlib.h>		/* for atof() */
+
+#define MAXOP 100		/* max size of operand or operator */
+#define NUMBER '0'		/* signal that a number was found */
+#define MATH_FUN '1'		/* signal that a math function was found */
+#define ASSIGNMENT '='		/* signal that a variable is to be assigned */
+#define VAR 'v'			/* signal that a variable was passed */
+
+char *line;				/* line buffer */
+size_t linecapp = MAXOP * sizeof(char);	/* maximum line length */
+
+int getop(char [], char[]);
+void push(double);
+double pop(void);
+
+void peek(void);		/* peek at top element of the stack: ? */
+void duplicate(void);		/* duplicate top element and push onto stack: @ */
+void swap(void);		/* swap top two elements: $ */
+void clear_stack(void);		/* clear the stack: _ */
+
+double resolve_fun(char []);	/* resolve math function */
+
+void assign_most_rec(double);   /* assign last printed variable to '~' */
+void assign_to_var(char);	/* assign variable */
+double get_var(char);		/* get variable */
+void print_var_array(void);	/* print the variable array */
+
+/* reverse Polish calculator */
+int main()
+{
+  int type;
+  double op2;
+  char s[MAXOP];
+  
+  if (getline(&line, &linecapp, stdin) < 1) {
+    printf("No input: terminating");
+    return 0;
+  }
+  
+  while ((type = getop(s, line)) != '\0') {
+    switch (type) {
+    case ASSIGNMENT:
+      assign_to_var(s[1]);
+      break;
+    case VAR:
+      push(get_var(s[0]));
+      break;
+    case MATH_FUN:
+      push(resolve_fun(s));
+      break;
+    case NUMBER:
+      push(atof(s));
+      break;
+    case '+':
+      op2 = pop();
+      push(pop() + op2);
+      break;
+    case '*':
+      op2 = pop();
+      push(pop() * op2);
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 != 0.0)
+	push(pop() / op2);
+      else
+	printf("error: zero divisor\n");
+      break;
+    case '%':
+      op2 = pop();
+      if(op2 != 0.0)
+	push((long) pop() % (long) op2); /* cannot do modulus on doubles */
+      else
+	printf("error: zero divisor\n");
+      break;
+    case '?':			/* peek at top element */
+      peek();
+      break;
+    case '@':			/* duplicate top element */
+      duplicate();
+      break;
+    case '$':			/* swap top two elements */
+      swap();
+      break;
+    case '_':			/* clear the stack */
+      clear_stack();
+      break;
+    case '\n':
+      assign_most_rec(pop());	/* store most recent printed value */
+      printf("\t%.8g\n", get_var('~'));
+      break;
+    default:
+      printf("error: unknown command %s\n", s);
+      break;
+    }
+  }
+  return 0;
+}
+
+/* ==========================STACK FUNCTIONS=============================== */
+#define MAXVAL 100		/* maximum depth of val stack */
+
+int sp = 0;			/* next free stack position */
+double val[MAXVAL];		/* value stack */
+
+/* push: push f onto value stack */
+void push(double f)
+{
+  if (sp < MAXVAL)
+    val[sp++] = f;
+  else
+    printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from the stack */
+double pop(void)
+{
+  if (sp > 0)
+    return val[--sp];
+  else {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
+
+/* get head of the stack */
+void peek(void)
+{
+  if (sp == 0)
+    printf("Stack empty\n");
+  else
+    printf("PEEKING: %f\n", val[sp-1]);
+  return;
+}
+
+/* duplicate top head of the stack and push it onto the stack */
+void duplicate(void)
+{
+  double dup = val[sp-1];
+  push(dup);
+}
+
+/* swap top two elements */
+void swap(void)
+{
+  double s1, s2;
+  s1 = pop();
+  s2 = pop();
+  push(s1);
+  push(s2);
+}
+
+/* clears everything from the stack.. acutally just changes the stack
+   pointer */
+void clear_stack(void)
+{
+  sp = 0;
+  return;
+}
+
+/* ==========================MATH FUNCTIONS=============================== */
+#include <math.h>
+#include <string.h>
+
+double pop(void);
+
+/* execute specified math functions */
+double resolve_fun(char s[])
+{
+  if (strcmp(s, "SIN") == 0)
+    return sin(pop());
+  if (strcmp(s, "EXP") == 0)
+    return exp(pop());
+  if (strcmp(s, "POW") == 0) {
+    double op = pop();
+    return pow(pop(), op);
+  }
+  printf("error: math function not found\n");
+  return -1;
+}
+
+/* ==========================INPUT HANDLING=============================== */
+#include <ctype.h>
+
+//int getch(void);
+//void ungetch(int);
+
+int p = 0;			/* index pointer */
+
+/* getop: get next operator or numeric operand  */
+int getop(char s[], char buf[])
+{
+  int i, c;
+  int assign=0;
+
+  while ((s[0] = c = buf[p++]) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+  i = 0;
+  if (c == '=')	{		/* handling assignment */
+    s[++i] = buf[p++];
+    return ASSIGNMENT;
+  }
+  if (!isdigit(c) && c != '.' && c != '-') {
+    if (islower(c) || c == '~')
+      return VAR;		/* handling variables */
+    if (isupper(c)) {
+      while (isupper(s[++i] = c = buf[p++]))
+	;
+      s[i] = '\0';
+      if (c != EOF)
+        p--;
+      return MATH_FUN;		/* math function */
+    }
+    return c;			/* not a number */
+  }
+  i = 0;
+  if (c == '-')			/* provisions for negative values */
+    c = s[++i] = buf[p++];
+  if (isdigit(c))		/* collect integer part */
+    while (isdigit(s[++i] = c = buf[p++]))
+      ;
+  if (c == '.')			/* collect fraction part */
+    while (isdigit(s[++i] = c = buf[p++]))
+      ;
+  s[i] = '\0';
+  p--;
+  return NUMBER;
+}
+
+/* ==========================BUFFER FUNCTIONS=============================== */
+int hold;			/* holding place for ungetch */
+int use=0;			/* indicate if hold is being used */
+
+/* get a (possibly pushed back) character */
+int getch(void)
+{
+  if (use) {
+    use = 0;
+    return hold;
+  }
+  else
+    return getchar();
+}
+
+/* push character back on input */
+void ungetch(int c)
+{
+  if (use)
+    printf("ungetch: too many characters\n");
+  else {
+    use = 0;
+    hold = c;
+  }
+}
+
+/* push back entire string onto input. Specifically the buffer */
+void ungets(char s[])
+{
+  for (int i=0; i != '\0'; i++)
+    ungetch(s[i]);
+  return;
+} 
+
+/* ==========================VAR ARRAY FUNCTIONS=============================== */
+double pop(void);
+
+double var_array[27];		/* array to hold variables a-z, last position
+				   holds most recent printed */
+
+/* assign a val in the last position in the var_array */
+void assign_most_rec(double val)
+{
+  var_array[26] = val;
+  return;
+}
+
+/* assign top of the stack to a given variable between a-z */
+void assign_to_var(char v)
+{
+  if (!islower(v)) {
+    printf("error: can only assign to variables between a-z\n");
+    return;
+  }
+  var_array[v - 'a'] = pop();
+  printf("Assigned %c=%f\n", v, var_array[v - 'a']);
+  return;
+}
+
+/* get specific variable value */
+double get_var(char v)
+{
+  if (v == '~')
+    return var_array[26];
+  else
+    return var_array[v-'a'];
+}
+
+/* printer for the variable array */
+void print_var_array(void)
+{
+  printf("|");
+  for (int i=0; i<27; i++)
+    printf(" %f ", var_array[i]);
+  printf("|\n");
+  return;
+}
--- Chapter4/4-5.c
+++ Chapter4/4-5.c
@@ -1,44 +1,25 @@
-// Exercise 4-3
-/*
-  Given the basic framwork. it's straightforward to extend the
-  calculator. Add the modulus (operator) and provisions for negative
-  numbers.
-*/
-
-// Exercise 4-4
-/* Add commands to print the top element of the stack without popping,
-   to duplicate it, and to swap the top elements. Add a command to
-   clear the stack.
-*/
-
 // Exercises 4-5
 /* Add access to library functions like sin, exp and pow. See <math.h>
-   in Appendix B, Section 4 */
+   in Appendix B, Section 4
+*/
 
 #include <stdio.h>
-#include <stdlib.h>       /* for atof() */
-#include <math.h>     /* for sin(x), exp(x) and pow(x, y) functions */
+#include <stdlib.h>		/* for atof() */
 
-#define MAXOP 100	         /* max size of operand or operator */
-#define NUMBER '0'	       /* signal that a number was found */
-
-#define PEEK 'P'
-#define DUPLICATE 'D'
-#define SWAP 'S'
-#define CLEAR 'C' 
-
-#define SIN '1'		       /* signal that sin(x) to be executed */
-#define EXP '2'		       /* signal that exp(x) to be executed */
-#define POW '3'		       /* signal that pow(x) to be executed */
+#define MAXOP 100		/* max size of operand or operator */
+#define NUMBER '0'		/* signal that a number was found */
+#define MATH_FUN '1'		/* signal that a math function was found */
 
 int getop(char []);
 void push(double);
 double pop(void);
-double peek(void);
-void duplicate(void);
-void swap(void);
-void clearstk(void);
 
+void peek(void);		/* peek at top element of the stack: ? */
+void duplicate(void);		/* duplicate top element and push onto stack: @ */
+void swap(void);		/* swap top two elements: $ */
+void clear_stack(void);		/* clear the stack: _ */
+
+double resolve_fun(char []);	/* resolve math function */
 
 /* reverse Polish calculator */
 int main()
@@ -49,6 +30,9 @@ int main()
 
   while ((type = getop(s)) != EOF) {
     switch (type) {
+    case MATH_FUN:
+      push(resolve_fun(s));
+      break;
     case NUMBER:
       push(atof(s));
       break;
@@ -74,41 +58,29 @@ int main()
     case '%':
       op2 = pop();
       if(op2 != 0.0)
-	push((int) pop() % (int) op2); /* Cannot do modulus on doubles */
+	push((long) pop() % (long) op2); /* cannot do modulus on doubles */
       else
 	printf("error: zero divisor\n");
       break;
-    case SIN:
-      push(sin(pop()));
+    case '?':			/* peek at top element */
+      peek();
       break;
-    case EXP:
-      push(exp(pop()));
-      break;
-    case POW:
-      op2 = pop();
-      push(pow(op2, pop()));
-      break;
-    case PEEK:
-      printf("\t%.8g\n", peek());
-      break;
-    case DUPLICATE:
+    case '@':			/* duplicate top element */
       duplicate();
       break;
-    case SWAP:
+    case '$':			/* swap top two elements */
       swap();
       break;
-    case CLEAR:
-      clearstk();
+    case '_':			/* clear the stack */
+      clear_stack();
       break;
     case '\n':
-      printf("\t%.8g\n", peek());
-      fflush(stdout);
+      printf("\t%.8g\n", pop());
       break;
     default:
       printf("error: unknown command %s\n", s);
       break;
     }
-    printf("PEEKING: %f\n", peek());
   }
   return 0;
 }
@@ -140,19 +112,19 @@ double pop(void)
 }
 
 /* get head of the stack */
-double peek(void)
+void peek(void)
 {
   if (sp == 0)
-    printf("Stack empty\n")
-    return 0.0;
+    printf("Stack empty\n");
   else
-    return val[sp-1];
+    printf("PEEKING: %f\n", val[sp-1]);
+  return;
 }
 
 /* duplicate top head of the stack and push it onto the stack */
 void duplicate(void)
 {
-  double dup = peek();
+  double dup = val[sp-1];
   push(dup);
 }
 
@@ -168,16 +140,35 @@ void swap(void)
 
 /* clears everything from the stack.. acutally just changes the stack
    pointer */
-void clearstk(void)
+void clear_stack(void)
 {
   sp = 0;
   return;
 }
 
+/* ==========================MATH FUNCTIONS=============================== */
+#include <math.h>
+#include <string.h>
+
+double pop(void);
+
+/* execute specified math functions */
+double resolve_fun(char s[])
+{
+  if (strcmp(s, "SIN") == 0)
+    return sin(pop());
+  if (strcmp(s, "EXP") == 0)
+    return exp(pop());
+  if (strcmp(s, "POW") == 0) {
+    double op = pop();
+    return pow(pop(), op);
+  }
+  printf("error: math function not found");
+  return -1;
+}
+
 /* ==========================INPUT HANDLING=============================== */
 #include <ctype.h>
-#include <string.h>   /* for strcmp */
-
 
 int getch(void);
 void ungetch(int);
@@ -186,66 +177,54 @@ void ungetch(int);
 int getop(char s[])
 {
   int i, c;
-  int paranthesis = 0;
 
   while ((s[0] = c = getch()) == ' ' || c == '\t')
     ;
   s[1] = '\0';
   i = 0;
-  if (c == '(') {		/* provision for negative numbers (-423) or (4040) allowed */
-    paranthesis = 1;
-    c = getch();
-    if (c == '-')
-      s[0] = c, c = getch(), s[1] = c, i = 1;
-    else
-      s[0] = c;
-  }
-  if (!isdigit(c) && c != '.') {
-    if (c == 'P' || c == 'D' || c == 'S' || c == 'C')       /* peek, duplicate, swap, clear functions */
-      return c;
-    char s[4];
-    s[0]=c, s[3]='\0';
-    s[1] = getchar();
-    s[2] = getchar();
-    if (strcmp(s, "sin") == 0)
-      return '1';
-    if (strcmp(s, "exp") == 0)
-      return '2';
-    if (strcmp(s, "pow") == 0)
-      return '3';
-    ungetch(s[2]);
-    ungetch(s[1]);
+  if (!isdigit(c) && c != '.' && c != '-') {
+    if (isupper(c)) {
+      while (isupper(s[++i] = c = getch()))
+	;
+      s[i] = '\0';
+      if (c != EOF)
+	ungetch(c);
+      return MATH_FUN;		/* math function */
+    }
     return c;			/* not a number */
   }
+  i = 0;
+  if (c == '-')			/* provisions for negative values */
+    c = s[++i] = getch();
   if (isdigit(c))		/* collect integer part */
     while (isdigit(s[++i] = c = getch()))
       ;
   if (c == '.')			/* collect fraction part */
     while (isdigit(s[++i] = c = getch()))
       ;
-  if (paranthesis) {
-    if (c != ')')
-      return c;
-    else
-      c = getch();
-  }
   s[i] = '\0';
   if (c != EOF)
     ungetch(c);
   return NUMBER;
 }
 
+
+
+
+/* ==========================BUFFER FUNCTIONS=============================== */
 #define BUFSIZE 100
 
 char buf[BUFSIZE];		/* buffer for ungetch */
 int bufp = 0;			/* next free position in buf */
 
-int getch(void)			/* get a (possibly pushed back) character */
+/* get a (possibly pushed back) character */
+int getch(void)
 {
   return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
-void ungetch(int c)		/* push character back on input */
+/* push character back on input */
+void ungetch(int c)
 {
   if (bufp >= BUFSIZE)
     printf("ungetch: too many characters\n");
